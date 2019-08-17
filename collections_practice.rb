31f4cf5f7f73aea7cde9c98fa4abeb5a6963d893
Warren Loy Harper II <warrenharper2@att.net>
2019-08-17 18:58:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,18 +5,5 @@ array.sort do |a, b|
   a <=> b
 end
   
-#question 2 
 
-array = [7, 3, 1, 2, 6, 5]
- 
-array.sort do |a, b|
-  if a == b
-    0
-  elsif a < b
-    +1
-  elsif a > b
-    -1
-  end
-end
 
-#question 3
