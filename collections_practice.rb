61eb1f0a644de2b594f8552027d20a9b893a9655
Warren Loy Harper II <warrenharper2@att.net>
2019-08-17 19:02:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,3 +41,13 @@ def find_a(array)
     sum+=num
   end
   sum
+  
+  def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
